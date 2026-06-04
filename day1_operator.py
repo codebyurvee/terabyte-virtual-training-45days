f263438bba0c4d3092b004a7d9f80ec955327cbe
Urvashi <urrvashii99@gmail.com>
2026-06-04 23:03:13 +0530

    Add arithmetic operations function
    Implement basic arithmetic operations based on user input.

---
+++ day1_operator.py
@@ -0,0 +1,23 @@
+#i useed assignment /relational operators 
+def arithemeticop():
+    a = int(input("enter a number"))
+    b = int(input("enter second number"))
+    choice = input("Enter a option(addition:+,subtraction:-,division:/,multiplication:*,exponent:^,modulus:%,floor division://)")
+    try:
+        if choice == '+':
+            print(a+b)
+        elif choice == '-':
+            print(a-b)
+        elif choice == '*':
+            print(a*b)
+        elif choice == '/':
+            print(a/b)
+        elif choice == '//':
+            print(a//b)
+        elif choice == '%':
+            print(a%b)
+        elif choice == '^':
+            print(a**b)     
+    except ValueError:
+        print("its an value error")
+arithemeticop()
