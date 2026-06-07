b79cf3ca1d389d8bd09da31af363da67fe9f0332
Urvashi <urrvashii99@gmail.com>
2026-06-07 23:45:09 +0530

    Add list, dictionary, set, and tuple examples

--- day4_datatypes.py
+++ day4_datatypes.py
@@ -6,7 +6,14 @@ _class = "BCA"
 roll_no = 36472
 is_student = True
 com = 3+4j
-
+li = [1,2,3,4,5]
+dic = {
+  "name": "urvshi",
+  "class": "BCA"
+}
+unique = {2,3,4,4,5
+         }
+marks =(80,89,99,79)
 #check type of variable
 
 print(type(name))
@@ -16,3 +23,10 @@ print(type(_class))
 print(type(roll_no))
 print(type(is_student))
 print(type(com))
+print(type(li))
+print(type(unique))
+print(type(marks))
+print(type(dic)
+
+
+
