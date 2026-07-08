da4c648047cf05bc140a86b53281ae4dc450f73a
Urvashi <urrvashii99@gmail.com>
2026-07-09 00:51:06 +0530

    Add tuple examples and operations in day13_tuple.py
    Added examples demonstrating tuple properties and operations.

--- day13_tuple.py
+++ day13_tuple.py
@@ -1,1 +1,15 @@
-
+# tuples are immutable and ordered data structure of python 
+tupy1 = (1,3,4,8,2)
+tupy = (7,6,4,8,2)
+print(type(tupy))
+print(tupy[1])
+print(tupy[-1])
+for t in tupy1:
+  print(t. end = "")
+for t in range(len(tupy1)):
+  print(tupy[t])
+subtp= tupy1[1:4]
+print(subtp)
+tup3 = tupy + tupy1
+print(tup3)
+print(tupy*3)
