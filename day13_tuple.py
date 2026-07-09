61d8cd2c2fefbe738670adf078fbe6614cdc7274
Urvashi <urrvashii99@gmail.com>
2026-07-09 16:21:35 +0530

    Correct print syntax and convert tuple to list
    Fix print statement syntax and add list conversion.

--- day13_tuple.py
+++ day13_tuple.py
@@ -1,3 +1,4 @@
+
 # tuples are immutable and ordered data structure of python 
 tupy1 = (1,3,4,8,2)
 tupy = (7,6,4,8,2)
@@ -5,7 +6,7 @@ print(type(tupy))
 print(tupy[1])
 print(tupy[-1])
 for t in tupy1:
-  print(t. end = "")
+  print(t, end = "")
 for t in range(len(tupy1)):
   print(tupy[t])
 subtp= tupy1[1:4]
@@ -13,3 +14,7 @@ print(subtp)
 tup3 = tupy + tupy1
 print(tup3)
 print(tupy*3)
+tup4 = list(tup3)
+tup4.append("hey")
+print(tup4)
+
