1b9adc385692136d04757a8b8a2878caa0f7ef84
Urvashi <urrvashii99@gmail.com>
2026-07-14 19:29:45 +0530

    Add calendar and time functionalities
    Added calendar and time functionalities to display month, current time, and character printing with delay. Implemented working hours check based on current time.

--- day20_datetime_.py
+++ day20_datetime_.py
@@ -1,1 +1,29 @@
 
+import calendar
+print(calendar.month(2026,7))
+print(calendar.prcal(2026))
+
+
+import time
+print(time.time())
+print(time.localtime(time.time()))
+print(time.asctime(time.localtime(time.time())))
+st = "Esha Verma"
+for s in st:
+    print(s)
+    time.sleep(1)
+    
+from datetime import datetime as dt 
+print(datetime.datetime(2026,7,14,8,42,30))
+
+if dt(dt.now().year,
+      dt.now().month,
+      dt.now().day,
+      8) < dt.now() < dt(dt.now().year,
+                          dt.now().month,
+                          dt.now().day,
+                          16):
+    print("Working hours....")
+else:
+    print("Fun hours")
+
